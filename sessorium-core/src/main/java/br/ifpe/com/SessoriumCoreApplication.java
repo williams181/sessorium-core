92be069fd1e4d26166d1e6a63b385ea13c450a93
william.roberto <william.roberto@healthx.com.br>
2021-11-19 08:36:21 -0300

    get started

---
+++ sessorium-core/src/main/java/br/ifpe/com/SessoriumCoreApplication.java
@@ -0,0 +1,13 @@
+package br.ifpe.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessoriumCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessoriumCoreApplication.class, args);
+	}
+
+}
---
+++ sessorium-core/src/test/java/br/ifpe/com/SessoriumCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifpe.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessoriumCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
